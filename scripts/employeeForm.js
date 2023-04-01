02d5827b0a6c2b8eaedd5babe69a53db07c51940
ShitalKarad <shitalkarad24@gmail.com>
2023-04-01 09:08:19 +0530

    Added: employee payroll form

---
+++ scripts/employeeForm.js
@@ -0,0 +1,21 @@
+const addEmployee=()=>{
+console.log("calling Employee function");
+ const name=document.getElementById('name').value 
+const fullName =$('#name').val()
+const profile =$('#profile1').val()
+const gender =$('#female').val()
+const department =$('#hr').val()
+const date =$('#date').val()
+const month =$('#salary').val()
+const year =$('#year').val()
+const note =$('#note').val()
+console.log(fullName);
+console.log(profile);
+console.log(gender);
+console.log(department);
+console.log(date);
+console.log(month);
+console.log(year);
+console.log(note);
+} 
+
