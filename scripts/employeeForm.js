768224ef558218792a02c8b9bb7069bbe8227588
ShitalKarad <shitalkarad24@gmail.com>
2023-04-12 00:42:28 +0530

    Added: Edit operation in dashbord

--- scripts/employeeForm.js
+++ scripts/employeeForm.js
@@ -1,21 +1,49 @@
-const addEmployee=()=>{
-console.log("calling Employee function");
- const name=document.getElementById('name').value 
-const fullName =$('#name').val()
-const profile =$('#profile1').val()
-const gender =$('#female').val()
-const department =$('#hr').val()
-const date =$('#date').val()
-const month =$('#salary').val()
-const year =$('#year').val()
-const note =$('#note').val()
-console.log(fullName);
-console.log(profile);
-console.log(gender);
-console.log(department);
-console.log(date);
-console.log(month);
-console.log(year);
-console.log(note);
-} 
+function employee(){
+	var name=$('#name').val();
+    console.log(name);
+
+    var image = $('input[name="profile"]:checked').val();
+  console.log(image);
+
+  var gender = $('input[name="gender"]:checked').val();
+  console.log(gender);
+
+  var department = $('input[name="department"]:checked').val();
+  console.log(department);
+
+  var salary = $('#salary').val();
+  console.log(salary);
+
+  var day = $('#date').val();
+  var month = $('#month').val();
+  var year = $('#year').val();
+  console.log("Start Date = " + day + month + year);
+
+  var note = $('#notes').val();
+  console.log(note);
+
+  let requestload = {
+    "name": name,
+    "profileUrl": image,
+    "gender": gender,
+    "departMent": department,
+    "salary": salary,
+    "startDate": day + ' ' + month + ' ' + year,
+    "notes": note
+  }
+  console.log(requestload);
+
+  $.ajax({
+    url: 'http://localhost:3000/employee',
+    type: 'POST',
+    dataType: 'json',
+    data: requestload,
+    success: function (data, textStatus, xhr) {
+      console.log(data);
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log('Error in Operation');
+    }
+  })
+}
 
